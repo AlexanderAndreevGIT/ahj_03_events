8d71b1b33d96971985660531c6602e377078976b
AlexanderAndreevGIT <andreev-alex@inbox.ru>
2022-02-17 01:07:23 +0300

    app.js fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,3 @@
-// import randomMinMaxNot from './utils';
-
-// function redrawPosition() {
-//   const board = Array.from(document.querySelectorAll('.cell'));
-//   const activeCell = board.find((el) => el.classList.contains('target'));
-//   const index = board.indexOf(activeCell);
-//   activeCell.classList.remove('target');
-//   board[randomMinMaxNot(0, 15, index)].classList.add('target');
-// }
-
-// setInterval(redrawPosition, 1000);
-
 import GamePlay from './GamePlay';
 import GameController from './GameController';
 
